8ec1e92b96778cad2114c11fe33f50d63b8c0016
CheolMin Lee <cheolmin.lee@gmail.com>
2018-06-28 12:09:31 +0900

    gkit module restructuring, added button, led control

--- python/gkit/__init__.py
+++ python/gkit/__init__.py
@@ -1,14 +1,14 @@
 """
-gkit
+gkit - AI Makers Kit Python Library
 """
 
 __title__ = 'gkit'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'CheolMin Lee'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2018 KT corp.'
 VERSION = tuple(map(int, __version__.split('.')))
 
-from .common import *
-import gigagenieRPC_pb2
-import gigagenieRPC_pb2_grpc
+from ._drivers import *
+from .kws import *
+from .grpc import *
