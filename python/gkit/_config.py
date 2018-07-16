3d3e236f669cab27f9e63589e93f6450e99332f0
CheolMin Lee <cheolmin.lee@gmail.com>
2018-07-16 18:20:12 +0900

    gkit.config

--- python/gkit/_config.py
+++ python/gkit/_config.py
@@ -1,10 +1,10 @@
 """ read config file: gkit.config """
 
 try:
-	# Python 3.x
+    # Python 3.x
     from configparser import ConfigParser
 except ImportError:
-	# Python 2.x
+    # Python 2.x
     from ConfigParser import SafeConfigParser as ConfigParser
 config = ConfigParser()
 config.read('gkit.config')
