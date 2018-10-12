8b75caa934c48df1a9a717eb65f42f57ec92b9ba
make1everything1hj <42664880+make1everything1hj@users.noreply.github.com>
2018-10-12 14:04:57 +0900

    DSS Parser Included Version

---
+++ nodejs/ex6_queryByVoice_Parser_Included.js
@@ -0,0 +1,63 @@
+const record=require('node-record-lpcm16');
+const aikit=require('./aimakerskitutil');
+const client_id='';
+const client_key='';
+const client_secret='';
+const json_path='';
+const cert_path='../data/ca-bundle.pem';
+const proto_path='../data/gigagenieRPC.proto';
+
+
+let dss_info = {
+	querytext: '',
+	app_id: '',
+	intent_name: '',
+	intent_answer: '',
+	app_info: ''
+};
+
+//aikit.initialize(client_id,client_key,client_secret,cert_path,proto_path);
+aikit.initializeJson(json_path,cert_path,proto_path);
+function initMic(){
+        return record.start({
+                sampleRateHertz: 16000,
+                threshold: 0,
+                verbose: false,
+                recordProgram: 'arecord',
+                silence: '10.0',
+        })
+};
+
+function JSON_Parser(msg){
+	dss_info.querytext = msg.uword;
+	user_info = msg.action;
+	tmp_info = JSON.stringify(user_info[0].serviceId);
+	
+	tmp_info = tmp_info.replace('"<![CDATA[', '');
+	tmp_info = tmp_info.replace(']]>"', '');
+	tmp_info =tmp_info.replace(/\\/g,'');
+	
+	var kit_info_json = JSON.parse(tmp_info);
+	dss_info.app_id = kit_info_json.VServiceID;
+	dss_info.intent_name = kit_info_json.Intent;
+	dss_info.intent_answer = kit_info_json.SystemMsg;
+	dss_info.app_info = JSON.stringify(kit_info_json.appinfo);
+	
+};
+
+
+const query=aikit.queryByVoice((err,msg)=>{
+	if(err){
+		console.log(JSON.stringify(err));
+	} else {
+		//console.log('Msg:'+JSON.stringify(msg));
+		JSON_Parser(msg);
+	}
+	record.stop();
+})
+query.write({reqOptions:{lang:0,userSession:'12345',deviceId:'D06190914TP808IQKtzq'}});
+const mic=initMic();
+mic.on('data',(data)=>{
+	query.write({audioContent:data});
+});
+console.log('say something');
